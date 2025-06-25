8c08cddbe2a89f9c425f423a72743d7c7f10dd38
Parvez-Shaik-6 <parvezsheikclg@gmail.com>
2025-06-25 23:36:47 +0530

    Update check.sh

--- customscripts/check.sh
+++ customscripts/check.sh
@@ -5,15 +5,17 @@ containers=("service1" "service2")
 
 # Alert function (customize as needed)
 send_alert() {
-  {
-    echo "Subject: $status alert
-    echo "To: "$EMAIL"
-    echo "From: $EMAIL"
-  }
   local container=$1
   local status=$2
 
-  echo "[ALERT] $container is $status at $(date)" >> docker_health_alerts.log  | msmtp "$EMAIL"
+  {
+    echo "Subject: $status alert"
+    echo "To: your@mail.com"
+    echo "From: your@mail.com"
+  }
+  
+
+  echo "[ALERT] $container is $status at $(date)" >> docker_health_alerts.log  | msmtp "your@mail.com"
   # You can replace this with a webhook call, email, etc.
 }
 
